9ad5e8cde7d561bb1d602c1bcaef67ab7b15b5e8
Murat Aydın <32421621+gfbmurat@users.noreply.github.com>
2021-06-16 02:02:52 +0300

    Project v.1.3
    Yorum Satırları Ekleme

--- FileConvert.java
+++ FileConvert.java
@@ -1,4 +1,5 @@
 import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessFile;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -7,9 +8,7 @@ import java.io.File;
 import java.io.PrintWriter;
 
 public class FileConvert {
-    
-    // FileConvert Sınıfı PDF Dosyayı Text'e Çevirmek İçin Yazılmıştır.
-    
+
     private String path;
     private String newFilePath;
     private String parsedText = null;
@@ -21,15 +20,21 @@ public class FileConvert {
         this.newFilePath = "C:/Users/aydn2/OneDrive/Masaüstü/pdf.txt";
     }
 
-    public String convertPDFToText(){
+    public void convertPDFToText(){
         try {
+            // PDF Yükleme
             File f = new File(this.getPath());
-            pdfParser = new PDFParser(new org.apache.pdfbox.io.RandomAccessFile(f,"r"));
+            pdfParser = new PDFParser(new RandomAccessFile(f,"r"));
             pdfParser.parse();
-            COSDocument cosDoc = pdfParser.getDocument();
+
+            // PDF Dosyasından Veriyi Alma
+            COSDocument cosDocument = pdfParser.getDocument();
             PDFTextStripper pdfTextStripper = new PDFTextStripper();
-            PDDocument pdDoc = new PDDocument(cosDoc);
-            parsedText = pdfTextStripper.getText(pdDoc);
+            // PDFTextStripper -> Ham metni almak
+            PDDocument pdDocument = new PDDocument(cosDocument);
+            parsedText = pdfTextStripper.getText(pdDocument);
+
+            // Veriyi aldıktan sonra text'e yazdırma,düzenleme ve bağlantı kapatma
             PrintWriter pw = new PrintWriter(newFilePath);
             parsedText.trim();
             pw.println(parsedText.trim());
@@ -37,7 +42,6 @@ public class FileConvert {
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
-        return this.getParsedText();
     }
 
     public String getPath() {
